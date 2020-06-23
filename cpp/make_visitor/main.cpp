8e9270d400c51293a6060247f859d272005f6964
Dreamykass <grunerdamian@gmail.com>
2020-06-23 21:28:20 +0200

    make_visitor

--- cpp/make_visitor/main.cpp
+++ cpp/make_visitor/main.cpp
@@ -2,7 +2,33 @@
 #include <iostream>
 #include <rang.hpp>
 
-int main() {
+#include <variant>
 
-  return 0;
+#include "make_visitor.hpp"
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+TEST_CASE("make_visitor") {
+  std::vector<std::string> strings;
+
+  auto visitor = make_visitor(
+    [&](int i) { strings.push_back("int " + std::to_string(i)); },
+    [&](float f) { strings.push_back("float " + std::to_string((int)(f))); },
+    [&](bool b) { strings.push_back("bool " + std::to_string(b)); });
+
+  std::variant<int, float, bool> variant;
+
+  variant = 2;
+  std::visit(visitor, variant);
+
+  variant = 33.0f;
+  std::visit(visitor, variant);
+
+  variant = true;
+  std::visit(visitor, variant);
+
+  CHECK(strings[0] == "int 2");
+  CHECK(strings[1] == "float 33");
+  CHECK(strings[2] == "bool 1");
 }
