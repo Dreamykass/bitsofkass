df8732d45a03ff489c0e109a9f3e199e12d45f08
Damian Grüner <grunerdamian@gmail.com>
2020-07-23 15:04:36 +0200

    fix cmake on gcpp

--- cpp/2020-07-23--hstutter-gcpp-test/help-paths-set.cmake
+++ cpp/2020-07-23--hstutter-gcpp-test/help-paths-set.cmake
@@ -1,3 +1,3 @@
 
 set(my_gcpp_path "${CMAKE_SOURCE_DIR}/../../../gcpp")
-set(my_gsl_path "${CMAKE_SOURCE_DIR}/../../../submodules/gsl/include")
+set(my_gsl_path "${CMAKE_SOURCE_DIR}/../../../gcpp/submodules/gsl/include")
