d10d10078b5f0c5dc28d977f73315f7e9c7f4d43
Damian Grüner <grunerdamian@gmail.com>
2020-07-23 12:26:49 +0200

    gcpp-start

---
+++ cpp/2020-07-23--hstutter-gcpp-test/help-paths-set.cmake
@@ -0,0 +1,3 @@
+
+set(my_gcpp_path "${CMAKE_SOURCE_DIR}/../../../gcpp")
+set(my_gsl_path "${CMAKE_SOURCE_DIR}/../../../submodules/gsl/include")
---
+++ cpp/2020-07-23--hstutter-gcpp-test/main.cpp
@@ -0,0 +1,6 @@
+
+#include <deferred_heap.h>
+
+int main() {
+  return 0;
+}
