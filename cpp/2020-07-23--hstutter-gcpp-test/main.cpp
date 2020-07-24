cf32a4e8a541ffab055e65ea62dfe29768ab7d39
Damian Grüner <grunerdamian@gmail.com>
2020-07-24 12:20:23 +0200

    gcpp again

--- cpp/2020-07-23--hstutter-gcpp-test/main.cpp
+++ cpp/2020-07-23--hstutter-gcpp-test/main.cpp
@@ -1,6 +1,42 @@
 
 #include <deferred_heap.h>
+#include <iostream>
+#include <string>
+#include <memory>
+
+void write(std::string _str) {
+  std::cout << _str;
+}
+
+class foo {
+public:
+  int data;
+  foo() { write("foo constructed\n"); }
+  ~foo() { write("foo destructed\n"); }
+};
 
 int main() {
+  write("start\n");
+
+  gcpp::deferred_heap dheap;
+
+  write("sizeof(deferred_ptr<foo>): " +
+        std::to_string(sizeof(dheap.make<foo>())) + "\n");
+  write("sizeof(unique_ptr<foo>): " +
+        std::to_string(sizeof(std::make_unique<foo>())) + "\n");
+  write("deferred heap created\n");
+
+  {
+    auto dptr = dheap.make<foo>();
+
+    write("deferred pointer created\n");
+
+    dptr->data;
+  }
+
+  write("deferred pointer destroyed\n");
+
+  write("returning from main\n");
+
   return 0;
 }
