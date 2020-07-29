382e1db03caf93da0baa78f950754f181d16cc2c
Damian Grüner <grunerdamian@gmail.com>
2020-07-29 17:39:19 +0200

    cmake-cpp-csharp-mixed

---
+++ mixed/2020-07-29--cmake_cpp-with-csharp/csharp-src/Main.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Hello from C#\n");
+            return 0;
+        }
+    }
+}
