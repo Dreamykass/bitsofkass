4913e5367b3d654732ece1656d54756ff052db9a
Damian Grüner <grunerdamian@gmail.com>
2020-08-13 13:47:06 +0200

    swarmz-sfml-v2

--- cpp/2020-08-13--swarmz-sfml-v2/main.cpp
+++ cpp/2020-08-13--swarmz-sfml-v2/main.cpp
@@ -13,23 +13,15 @@ int main() {
     auto config = toml::parse_file("config.toml");
     NiceRand randEngine(42);
 
-    // read the boids from config.toml
+    // generate the boids
     std::vector<sw::Boid> boids;
-    for (const auto& bo : *config["Boids"].as_array()) {
-      const auto& tabl = *bo.as_table();
-      const auto& pos = *tabl["pos"].as_array();
-      const auto& vel = *tabl["vel"].as_array();
-
-      float pos1 = pos[0].as_floating_point()->get();
-      float pos2 = pos[1].as_floating_point()->get();
-      float pos3 = pos[2].as_floating_point()->get();
-      float vel1 = vel[0].as_floating_point()->get();
-      float vel2 = vel[1].as_floating_point()->get();
-      float vel3 = vel[2].as_floating_point()->get();
-
+    auto boidAmount = config["BoidAmount"].as_integer()->get();
+    for (int64_t i = 0; i < boidAmount; i++) {
+      auto r = [&]() { return randEngine.GetRealUniform(-400.f, 400.f); };
       boids.push_back(
-        sw::Boid(sw::Vec3(pos1, pos2, pos3), sw::Vec3(vel1, vel2, vel3)));
+        sw::Boid(sw::Vec3(r(), r(), r()), sw::Vec3(r(), r(), r())));
     }
+
     float randomnessDeviation =
       config["RandomnessDeviation"].as_floating_point()->get();
 
@@ -48,8 +40,11 @@ int main() {
     sf::RenderWindow window(sf::VideoMode(800, 800), "SWARMZ works!");
     window.setFramerateLimit(60);
     sf::CircleShape shape(config["ShapeRadius"].as_floating_point()->get());
-    shape.setFillColor(sf::Color::Green);
+    shape.setFillColor(sf::Color::Red);
     sf::View view;
+    view.setCenter(0.f, 0.f);
+    view.setSize(800.f, 800.f);
+    window.setView(view);
 
     // -------------------------------------------------------------------------
     // main update and render loop
@@ -66,24 +61,24 @@ int main() {
 
       swarm.Update(deltaSeconds);
 
-      sf::Vector2f posMax = { 0.f, 0.f };
-      sf::Vector2f posMin = { 0.f, 0.f };
-
       for (auto& boid : boids) {
         boid.Velocity.Z = 0.f;
         boid.Position += boid.Velocity * deltaSeconds;
         boid.Position.Z = 0.f;
         boid.Acceleration.Z = 0.f;
 
-        //   boid.Velocity.X = randEngine.GetRealUniform(-1.f, 1.f);
-        //   boid.Velocity.Y = randEngine.GetRealUniform(-1.f, 1.f);
-        boid.Velocity.X = randEngine.GetRealNormal(0.f, randomnessDeviation);
-        boid.Velocity.Y = randEngine.GetRealNormal(0.f, randomnessDeviation);
+        while (boid.Position.X < -400.f)
+          boid.Position.X += 800.f;
+        while (boid.Position.X > 400.f)
+          boid.Position.X -= 800.f;
 
-        posMax.x = std::max(posMax.x, boid.Position.X);
-        posMax.y = std::max(posMax.y, boid.Position.Y);
-        posMin.x = std::min(posMin.x, boid.Position.X);
-        posMin.y = std::min(posMin.y, boid.Position.Y);
+        while (boid.Position.Y < -400.f)
+          boid.Position.Y += 800.f;
+        while (boid.Position.Y > 400.f)
+          boid.Position.Y -= 800.f;
+
+        boid.Velocity.X += randEngine.GetRealNormal(0.f, randomnessDeviation);
+        boid.Velocity.Y += randEngine.GetRealNormal(0.f, randomnessDeviation);
 
         shape.setPosition({ boid.Position.X, boid.Position.Y });
         window.draw(shape);
@@ -104,15 +99,6 @@ int main() {
           config["OnKeyVelocityChange"].as_floating_point()->get();
 
       // view and display
-      // float actualViewSize = std::max(posMax.x + posMin.x, posMax.y +
-      // posMin.y); view.setSize(actualViewSize * 1.4f, actualViewSize * 1.4f);
-      view.setSize(config["ViewSize"].as_floating_point()->get(),
-                   config["ViewSize"].as_floating_point()->get());
-      view.setCenter(boids[0].Position.X, boids[0].Position.Y);
-      window.setView(view);
-
-      std::cout << view.getCenter().x << "/" << view.getCenter().y << "\n";
-
       window.display();
     }
   } catch (const std::exception& e) {
