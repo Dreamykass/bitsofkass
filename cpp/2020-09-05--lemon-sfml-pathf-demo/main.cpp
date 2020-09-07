423d2a0d55693de870d5a5d9fc4b33558497d72e
Damian Grüner <grunerdamian@gmail.com>
2020-09-07 21:19:00 +0200

    lemon-sfml-pathf-demo

---
+++ cpp/2020-09-05--lemon-sfml-pathf-demo/main.cpp
@@ -0,0 +1,13 @@
+
+#include <lemon/list_graph.h>
+#include <SFML/Graphics.hpp>
+
+int main() {
+
+  lemon::ListDigraph graph;
+
+  // generate nodes and arcs
+  { auto node = graph.addNode(); }
+
+  return 0;
+}
