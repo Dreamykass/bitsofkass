2a5546537e175eabfdb1240fec3f48c5bb0ec0d9
Damian Grüner <grunerdamian@gmail.com>
2021-01-27 17:23:01 +0100

    tarkov done

--- rs/2021-01-26--tarkov-save-money-cheat/tarkov/src/main.rs
+++ rs/2021-01-26--tarkov-save-money-cheat/tarkov/src/main.rs
@@ -1,10 +1,82 @@
+use serde_json::{Number, Value};
+use std::fs::{create_dir_all, File};
+use std::io::{Read, Write};
 use std::path::Path;
 
-const FOO: &str =
-    "G:/Torrents/Escape.from.Tarkov/EFT-SP_A7-SvadilfariUnlock/AKI-Server-A7/user/profiles";
-
 fn main() {
-    println!("Hello, world!");
+    let mut profiles = "".to_string();
+    File::open(Path::new("config.json"))
+        .unwrap()
+        .read_to_string(&mut profiles)
+        .unwrap();
 
-    let path = Path::new("lorem_ipsum.txt");
+    println!("loaded profiles: ");
+    let profiles = serde_json::from_str::<Value>(&*profiles)
+        .unwrap()
+        .get("profiles")
+        .unwrap()
+        .as_array()
+        .unwrap()
+        .iter()
+        .map(|v| v.as_str().unwrap().to_string())
+        .inspect(|s| println!("  {}", s))
+        .collect::<Vec<_>>();
+
+    create_dir_all("_backup").unwrap();
+    create_dir_all("_output").unwrap();
+    println!();
+    println!("created folders for backups and outputs");
+
+    for profile in profiles {
+        let profile_path = profile;
+        let mut profile_raw_json_str = "".to_string();
+
+        let profile_filename = Path::new(&profile_path)
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .to_string();
+
+        File::open(Path::new(&*profile_path))
+            .unwrap()
+            .read_to_string(&mut profile_raw_json_str)
+            .unwrap();
+
+        let profile_raw_json_str = profile_raw_json_str;
+        File::create(format!("_backup/{}", &profile_filename))
+            .unwrap()
+            .write_all(profile_raw_json_str.as_bytes())
+            .unwrap();
+        println!("created backup: {}", profile_filename);
+
+        let mut profile_json = serde_json::from_str::<Value>(&*profile_raw_json_str).unwrap();
+
+        #[allow(clippy::suspicious_map)]
+        let count = profile_json
+            .pointer_mut("/characters/pmc/Inventory/items")
+            .unwrap()
+            .as_array_mut()
+            .unwrap()
+            .iter_mut()
+            .filter(|item| {
+                item.get("_tpl").unwrap().as_str().unwrap() == "5449016a4bdc2d6f028b456f"
+            })
+            .map(|item| {
+                let item = item.get_mut("upd").unwrap().as_object_mut().unwrap();
+                item["StackObjectsCount"] = Value::Number(Number::from(500_000));
+            })
+            .count();
+
+        println!("Modified this many stacks: {}", count);
+
+        File::create(format!("_output/{}", &profile_filename))
+            .unwrap()
+            .write_all(
+                serde_json::ser::to_string_pretty(&profile_json)
+                    .unwrap()
+                    .as_bytes(),
+            )
+            .unwrap();
+    }
 }
