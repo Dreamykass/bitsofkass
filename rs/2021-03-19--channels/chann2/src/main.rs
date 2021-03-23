bf8b535ea390d2c13368757db0cfe93b0774b99b
Damian Grüner <grunerdamian@gmail.com>
2021-03-23 13:32:31 +0100

    channel stuff

--- rs/2021-03-19--channels/chann2/src/main.rs
+++ rs/2021-03-19--channels/chann2/src/main.rs
@@ -14,47 +14,34 @@ fn time_begin_period() {
     }
 }
 
-const DURATION: Duration = Duration::from_millis(1);
-
 fn main() {
     println!("Hello, world!");
     time_begin_period();
 
-    let timeout = after(Duration::from_millis(40));
+    let timeout = after(Duration::from_millis(40000));
 
-    let mut ticker_start = Instant::now();
-    let ticker = tick(DURATION);
+    let ticker_start = Instant::now();
+    let ticker = tick(Duration::from_millis(1));
 
-    let mut std_sleep_start = Instant::now();
+    let std_sleep_start = Instant::now();
     let (std_sleep_sender, std_sleep) = unbounded::<()>();
     std::thread::spawn(move || loop {
-        std::thread::sleep(DURATION);
+        std::thread::sleep(Duration::from_millis(1));
         let _ = std_sleep_sender.send(());
     });
 
-    let mut spin_sleep_start = Instant::now();
+    let spin_sleep_start = Instant::now();
     let (spin_sleep_sender, spin_sleep) = unbounded::<()>();
     std::thread::spawn(move || loop {
-        // spin_sleep::sleep(Duration::from_millis(1));
-        let spin_sleeper = spin_sleep::SpinSleeper::new(2_000_000); // 2ms
-        spin_sleeper.sleep(DURATION);
+        spin_sleep::sleep(Duration::from_millis(1));
         let _ = spin_sleep_sender.send(());
     });
 
     loop {
         select! {
-            recv(ticker) -> _ => {
-                println!("elapsed in ticker: {:?}", ticker_start.elapsed());
-                ticker_start = Instant::now();
-            },
-            recv(std_sleep) -> _ => {
-                println!("elapsed in std_sleep: {:?}", std_sleep_start.elapsed());
-                std_sleep_start = Instant::now();
-            },
-            recv(spin_sleep) -> _ => {
-                println!("elapsed in spin_sleep: {:?}", spin_sleep_start.elapsed());
-                spin_sleep_start = Instant::now();
-            },
+            recv(ticker) -> _ => println!("elapsed in ticker: {:?}", ticker_start.elapsed().as_millis()),
+            // recv(std_sleep) -> _ => println!("elapsed in std_sleep: {:?}", std_sleep_start.elapsed()),
+            // recv(spin_sleep) -> _ => println!("elapsed in spin_sleep: {:?}", spin_sleep_start.elapsed()),
             recv(timeout) -> _ => break,
             recv(never::<()>()) -> _ => {},
         }
