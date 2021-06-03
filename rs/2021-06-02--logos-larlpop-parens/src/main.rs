a5124cff39b82d297064d6cf3adc8bd98e9dd362
Damian Grüner <grunerdamian@gmail.com>
2021-06-04 00:22:12 +0200

    logos-larlpop-parens json

--- rs/2021-06-02--logos-larlpop-parens/src/main.rs
+++ rs/2021-06-02--logos-larlpop-parens/src/main.rs
@@ -1,8 +1,7 @@
-use logos::Logos;
-// #[macro_use]
-// extern crate lalrpop_util;
-
 use lalrpop_util::lalrpop_mod;
+use logos::Logos;
+use serde::{Deserialize, Serialize};
+
 lalrpop_mod!(pub numbers);
 
 #[derive(logos::Logos, Debug, PartialEq, Clone, Copy)]
@@ -24,7 +23,7 @@ pub enum LogosToken {
     Error,
 }
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
 pub enum Token {
     ParenLeft,
     ParenRight,
@@ -32,7 +31,7 @@ pub enum Token {
     Number(i32),
 }
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
 pub enum SingleTokenOrVec {
     TokenSingle(Token),
     TokenVec(Vec<SingleTokenOrVec>),
@@ -69,6 +68,9 @@ fn main() {
     println!("\nparsed result:");
     let par = numbers::CompleteParser::new().parse(lexed.into_iter().map(|x| x));
     println!("  {:?}", par);
+
+    println!("\njson:");
+    println!("{}", serde_json::to_string_pretty(&par.unwrap()).unwrap());
 
     println!("\n");
 }
