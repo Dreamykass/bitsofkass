be9355d5e492fb6713a43db0b84976cfc29df06a
Damian Grüner <grunerdamian@gmail.com>
2021-06-23 23:09:39 +0200

    qt creator hello world

---
+++ cpp-qt/2021-06-23--qt-creator-hello-world/mainwindow.cpp
@@ -0,0 +1,23 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include <iostream>
+#include <string>
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_pressed()
+{
+    std::cerr << "button pressed\n";
+    this->counter_value++;
+    auto *textEdit = this->findChild<QTextEdit *>("textEdit");
+    Q_ASSERT(textEdit);
+    textEdit->setText(QString::fromStdString(std::to_string(this->counter_value)));
+}
