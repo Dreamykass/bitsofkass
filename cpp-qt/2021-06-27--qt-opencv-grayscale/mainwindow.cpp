665736d29f3e4ddb23ea6396b1ad47fb32532395
Damian Grüner <grunerdamian@gmail.com>
2021-06-27 16:28:11 +0200

    qt opencv grayscale

--- cpp-qt/2021-06-27--qt-opencv-grayscale/mainwindow.cpp
+++ cpp-qt/2021-06-27--qt-opencv-grayscale/mainwindow.cpp
@@ -1,9 +1,12 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QFileDialog>
+#include <QtDebug>
+#include <QPixmap>
+#include <opencv2/opencv.hpp>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-      , ui(new Ui::MainWindow)
+    : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 }
@@ -13,3 +16,31 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::on_pushButton_pressed()
+{
+    auto fileName = QFileDialog::getOpenFileName(this, tr("Open Image"), "C:/Users/grune/Downloads", tr("Image Files (*.png *.jpg *.bmp)"));
+    qDebug() << "filename: " << fileName;
+
+    auto *button = this->findChild<QPushButton *>("pushButton");
+
+    auto cv_image = cv::imread(fileName.toStdString());
+    if (!cv_image.empty())
+    {
+        qDebug() << "cv_image is ok";
+        qDebug() << "rows: " << cv_image.cols << "; cols:" << cv_image.rows;
+
+        cv::cvtColor(cv_image, cv_image, cv::COLOR_BGR2GRAY);
+        cv::cvtColor(cv_image, cv_image, cv::COLOR_GRAY2RGBA);
+        qDebug() << "converted...";
+
+        auto qt_image = QImage(cv_image.data, cv_image.cols, cv_image.rows, QImage::Format_RGBA8888);
+        auto pixmap = QPixmap::fromImage(qt_image);
+        button->setIcon(pixmap);
+        button->setIconSize(QSize(600, 600));
+        qDebug() << "done";
+    }
+    else
+    {
+        qDebug() << "cv_image is empty";
+    }
+}
--- cpp-qt/2021-06-27--qt-opencv-grayscale/readme.md
+++ cpp-qt/2021-06-27--qt-opencv-grayscale/readme.md
@@ -1,7 +1,18 @@
 
+This shows a qt gui that asks to load an image, 
+which is then loaded with opencv, 
+which transforms the image to greyscale,
+and then the image is displayed in the qt gui.
+
+
+The fun part happens in `mainwindow.cpp`
+
+1. create project in qtcreator
+2. copy this to `.vscode/settings.json`
 ```json
 {
     "cmake.configureSettings": {
+        "OpenCV_DIR": "D:/Code/__cpp-libs/opencv/build",
         "Qt6_DIR": "C:/QT/6.1.1/msvc2019_64/lib/cmake/Qt6",
         "Qt6Widgets_DIR": "C:/QT/6.1.1/msvc2019_64/lib/cmake/Qt6Widgets",
         "Qt6WidgetsTools_DIR": "C:/QT/6.1.1/msvc2019_64/lib/cmake/Qt6WidgetsTools",
@@ -10,3 +21,16 @@
     },
 }
 ```
+3. add this to cmakelists.txt (and `${UIS_HDRS}` to `qt_add_executable()`)
+```cmake
+qt6_wrap_ui(UIS_HDRS
+    mainwindow.ui
+)
+```
+5. add this to cmakelists.txt where relevant
+```cmake
+find_package(OpenCV REQUIRED)
+include_directories(${OpenCV_INCLUDE_DIRS})
+target_link_libraries(opencv_hello_world ${OpenCV_LIBS})
+```
+6. copy the dlls
